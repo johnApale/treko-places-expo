4d95b44361742f069cf5ab28fc67f8e4cf2b66cd
John Apale <74929433+johnApale@users.noreply.github.com>
2023-08-23 19:36:13 -0700

    Initialized Supabase

---
+++ lib/supabase.ts
@@ -0,0 +1,28 @@
+import "react-native-url-polyfill/auto";
+import * as SecureStore from "expo-secure-store";
+import { createClient } from "@supabase/supabase-js";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const ExpoSecureStoreAdapter = {
+  getItem: (key: string) => {
+    return SecureStore.getItemAsync(key);
+  },
+  setItem: (key: string, value: string) => {
+    SecureStore.setItemAsync(key, value);
+  },
+  removeItem: (key: string) => {
+    SecureStore.deleteItemAsync(key);
+  },
+};
+
+const supabaseUrl = process.env.SUPABASE_URL as string;
+const supabaseAnonKey = process.env.SUPABASE_ANON_KEY as string;
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: ExpoSecureStoreAdapter as any,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+});
---
+++ src/contexts/AuthProvider.tsx
@@ -0,0 +1,72 @@
+import React, { createContext, useContext, useState, useEffect } from "react";
+import { supabase } from "../../lib/supabase";
+import { Session, User } from "@supabase/supabase-js";
+type ContextProps = {
+  user: User | null | undefined;
+  session: Session | null;
+  isLoading: boolean;
+};
+
+const AuthContext = createContext<Partial<ContextProps>>({});
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const AuthProvider = (props: Props) => {
+  // user null = loading
+  const [user, setUser] = useState<User | null | undefined>(null);
+  const [session, setSession] = useState<Session | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const setData = async () => {
+      const {
+        data: { session },
+        error,
+      } = await supabase.auth.getSession();
+      if (error) throw error;
+      if (session) {
+        setSession(session);
+        setUser(session?.user);
+        setIsLoading(false);
+      }
+    };
+
+    const { data: listener } = supabase.auth.onAuthStateChange(
+      async (event, session) => {
+        console.log(`Supabase auth event: ${event}`);
+        setSession(session);
+        setUser(session?.user);
+        setIsLoading(false);
+      }
+    );
+
+    setData();
+
+    return () => {
+      listener?.subscription.unsubscribe();
+    };
+  }, [user]);
+
+  return (
+    <AuthContext.Provider
+      value={{
+        user,
+        session,
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
+
+const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuth must be used within an AuthProvider");
+  }
+  return context;
+};
+
+export { AuthContext, AuthProvider, useAuth };
