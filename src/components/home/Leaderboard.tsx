10871d580c36fee1463090032d4b37ce5000cf91
John Apale <74929433+johnApale@users.noreply.github.com>
2023-08-28 22:31:05 -0700

    Added temporary leaderboard screen

--- src/components/home/Leaderboard.tsx
+++ src/components/home/Leaderboard.tsx
@@ -1,10 +1,18 @@
 import { View, Text } from "react-native";
 import React from "react";
+import ScreenTitle from "../shared/ScreenTitle";
 
 const Leaderboard = () => {
   return (
-    <View>
-      <Text>Leaderboard</Text>
+    <View
+      style={{
+        paddingHorizontal: 15,
+        width: "100%",
+        height: "80%",
+        justifyContent: "center",
+      }}
+    >
+      <ScreenTitle title="Coming soon..." textAlign="center" />
     </View>
   );
 };
